54dba916fcbe966f3936e54144a97359b45a1ce0
Jay Cabasag <jaycabasag1999@gmail.com>
2023-06-22 23:32:37 +0800

    Added rooms resource

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
-import { RoomSchema } from './schemas/room.schema';
-import { MongooseModule } from '@nestjs/mongoose';
+import { RoomsController } from './rooms.controller';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'Room', schema: RoomSchema }])
-  ],
   controllers: [RoomsController],
   providers: [RoomsService]
 })
-export class RoomsModule { }
+export class RoomsModule {}
