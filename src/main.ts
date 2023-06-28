2a8d0e61b375890a869a3f970aefeab23d2d6131
Jay Cabasag <jaycabasag1999@gmail.com>
2023-06-29 01:24:35 +0800

    Added new changes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 const corsOptios = {
   origin: ['http://localhost:4200'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type, Accept',
+  allowedHeaders: 'Content-Type, Accept, Authorization',
   credentials: true,
 }
 
